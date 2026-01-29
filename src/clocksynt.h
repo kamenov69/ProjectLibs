0b818cf89fe1aa1c1990d0207b0c50b5d180a4e0
kamenov69 <kamen.p.kamenov@gmail.com>
2026-01-29 22:34:04 +0200

    added SI5351 clock sytesator

---
+++ src/clocksynt.h
@@ -0,0 +1,6 @@
+#ifndef __CLOCKSYNT__
+#define __CLOCKSYNT__
+void setup_clocksynt();
+void loop_clocksynt();
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
 #include "board.h"
 
+#include "clocksynt.h"
 
 
 void setup() {
     setup_board();
+    setup_clocksynt();
+
 }
 
 void loop() {
    loop_board();
+   loop_clocksynt();
 }
 
+ 
