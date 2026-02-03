c2a2452f1fe498ac3cf883490102d58aebcd7268
kamenov69 <kamen.p.kamenov@gmail.com>
2026-02-03 09:05:51 +0200

    v01.0

--- src/clocksynt.h
+++
@@ -1,6 +0,0 @@
-#ifndef __CLOCKSYNT__
-#define __CLOCKSYNT__
-void setup_SI5351_clck_synt();
-void loop_clocksynt();
-
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,9 @@
 #include <Arduino.h>
 #include "board.h"
-
-#include "clocksynt.h"
 #include "globalVars.h"
 
 void setup() {
     setup_board();
-    setup_SI5351_clck_synt();
     
     add_new_global_var("glo",1,10,10);
     
@@ -15,8 +12,6 @@ void setup() {
 
 void loop() {
    loop_board();
-   loop_clocksynt();
-
 }
 
  
